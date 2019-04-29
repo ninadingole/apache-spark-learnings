fcdc5e7cfa76f9f321a46691c7374be10f0748e0
Ninad Ingole <robert.wallis@example.com>
2019-04-29 20:27:37 +0530

    Update AccountBalance To Select Max Date for Position, Add Test

--- spark-learnings/src/main/scala/com/learn/spark/grouping/AccountBalanceProcessor.scala
+++ spark-learnings/src/main/scala/com/learn/spark/grouping/AccountBalanceProcessor.scala
@@ -1,56 +1,18 @@
 package com.learn.spark.grouping
 
-import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
-import org.apache.spark.sql.{Row, SparkSession}
 
-import scala.collection.JavaConverters._
+class AccountBalanceProcessor {
 
-object AccountBalanceProcessor {
-  val row1: Array[Any] = Array("123456789", "1550620800000") // right
-  val row2: Array[Any] = Array("123456789", "1550361600000")
-  val row3: Array[Any] = Array("123456789", "1547251200000") // right
-  val row4: Array[Any] = Array("123456789", "1549843200000")
-  val row5: Array[Any] = Array("987654321", "1550620800000") // right
-  val row6: Array[Any] = Array("987654321", "1550361600000")
-  val row7: Array[Any] = Array("987654321", "1546992000000") // right
-  val row8: Array[Any] = Array("987654321", "1543622400000") // right
-
-  def main(args: Array[String]): Unit = {
-
-    val schema = StructType(
-      Seq(StructField("acct_key", DataTypes.StringType, false),
-          StructField("posn_dt", DataTypes.StringType, false)))
-
-    val list: java.util.List[Row] = List(
-      new GenericRowWithSchema(row2, schema).asInstanceOf[Row],
-      new GenericRowWithSchema(row1, schema).asInstanceOf[Row],
-      new GenericRowWithSchema(row3, schema).asInstanceOf[Row],
-      new GenericRowWithSchema(row4, schema).asInstanceOf[Row],
-      new GenericRowWithSchema(row5, schema).asInstanceOf[Row],
-      new GenericRowWithSchema(row6, schema).asInstanceOf[Row],
-      new GenericRowWithSchema(row7, schema).asInstanceOf[Row],
-      new GenericRowWithSchema(row8, schema).asInstanceOf[Row]
-    ).asJava
-
-    val session = SparkSession
-      .builder()
-      .appName("accountBalance")
-      .master("local[2]")
-      .getOrCreate()
-
-    val df = session.createDataFrame(list, schema)
+  def process(df: DataFrame): DataFrame = {
 
     df.repartition(col("acct_key"))
       .withColumn("yearandmonth",
                   from_unixtime(col("posn_dt").divide(1000), "yyyy-MM"))
-      .orderBy(desc("posn_dt"))
       .groupBy(col("yearandmonth"), col("acct_key"))
       .agg(first("posn_dt").as("posn_dt"))
       .select(col("acct_key"), col("posn_dt"))
-      .show(false)
-    session.close()
-
   }
+
 }
---
+++ spark-learnings/src/test/scala/com/learn/spark/grouping/AccountBalanceProcessorTest.scala
@@ -0,0 +1,60 @@
+package com.learn.spark.grouping
+
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
+import org.scalatest.{Matchers, WordSpec}
+
+import scala.collection.JavaConverters._
+
+class AccountBalanceProcessorTest
+    extends WordSpec
+    with DataFrameSuiteBase
+    with Matchers {
+
+  import AccountBalanceProcessorTest._
+
+  "AccountBalanceProcessor" should {
+    "only select max date rows" in {
+      val df = spark.createDataFrame(list, schema)
+      val resultDF = new AccountBalanceProcessor().process(df)
+      val rows = resultDF.collect()
+      println(rows)
+    }
+  }
+
+}
+
+object AccountBalanceProcessorTest {
+  private val row1
+    : Array[Any] = Array("123456789", "1488412800000") // 2-MAR-17 right
+  private val row2: Array[Any] = Array("123456789", "1488326400000") // 1-MAR-17
+  private val row3
+    : Array[Any] = Array("123456789", "1547251200000") // 12-JAN-19 right
+  private val row4
+    : Array[Any] = Array("123456789", "1549843200000") // 11-FEB-19
+  private val row5
+    : Array[Any] = Array("987654321", "1550620800000") // 20-FEB-19 right
+  private val row6
+    : Array[Any] = Array("987654321", "1550361600000") // 17-FEB-19
+  private val row7
+    : Array[Any] = Array("987654321", "1546992000000") // 9-JAN-19 right
+  private val row8
+    : Array[Any] = Array("987654321", "1543622400000") // 1-DEC-18 right
+
+  private val schema = StructType(
+    Seq(StructField("acct_key", DataTypes.StringType, nullable = false),
+        StructField("posn_dt", DataTypes.StringType, nullable = false)))
+
+  private val list: java.util.List[Row] = List(
+    new GenericRowWithSchema(row2, schema).asInstanceOf[Row],
+    new GenericRowWithSchema(row1, schema).asInstanceOf[Row],
+    new GenericRowWithSchema(row3, schema).asInstanceOf[Row],
+    new GenericRowWithSchema(row4, schema).asInstanceOf[Row],
+    new GenericRowWithSchema(row5, schema).asInstanceOf[Row],
+    new GenericRowWithSchema(row6, schema).asInstanceOf[Row],
+    new GenericRowWithSchema(row7, schema).asInstanceOf[Row],
+    new GenericRowWithSchema(row8, schema).asInstanceOf[Row]
+  ).asJava
+}
